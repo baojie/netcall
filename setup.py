59178ab636e61d263ea9ac50df1c4df85f18c8d1
Alexander Glyzov <bonoba@gmail.com>
2014-03-05 20:38:11 +0200

    removed tornado from strong requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version = "0.2.7",
     packages = find_packages(),
 
-    install_requires = ['pyzmq','tornado'],
+    install_requires = ['pyzmq'],
 
     author = "Alexander Glyzov",
     author_email = "bonoba@gmail.com",
