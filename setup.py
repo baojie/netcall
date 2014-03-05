9ee34af92a3f06e5aceb441704399a3a586b4b4e
Alexander Glyzov <bonoba@gmail.com>
2014-03-05 20:36:49 +0200

    bumped version to 0.2.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "netcall",
-    version = "0.2.6",
+    version = "0.2.7",
     packages = find_packages(),
 
     install_requires = ['pyzmq','tornado'],
